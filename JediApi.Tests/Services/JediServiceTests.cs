a92d4dba6e99a07ccad16ca63d49baecb3525370
viniciuscgama <viniciuscgama@hotmail.com>
2024-06-18 22:11:39 -0300

    First commit

---
+++ JediApi.Tests/Services/JediServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using JediApi.Models;
+using JediApi.Repositories;
+using JediApi.Services;
+using Moq;
+
+namespace JediApi.Tests.Services
+{
+    public class JediServiceTests
+    {
+        // não mexer
+        private readonly JediService _service;
+        private readonly Mock<IJediRepository> _repositoryMock;
+
+        public JediServiceTests()
+        {
+            // não mexer
+            _repositoryMock = new Mock<IJediRepository>();
+            _service = new JediService(_repositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_Success()
+        {
+            var jedi = new jedi { Id = 1, Name = "VinicinDoMal", Strength = 100, Version = 1 };
+            _repositoryMock.Setup(r => r.GetByIdAsync(1)).ReturnsAsync(jedi);
+
+            var result = await _service.GetByIdAsync(1);
+
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task GetById_NotFound()
+        {
+            var result = await _service.GetByIdAsync(1);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetAll()
+        {
+            var jediMuitos = new List<jedi>{
+            new jedi{Id = 1, Name = "VinicinDoMal", Strength = 100, Version = 1 },
+            new jedi{Id = 2, Name = "VinicinDoBem", Strength = 110, Version = 1 },
+            new jedi{Id = 3, Name = "VinicinDoLOL", Strength = 120, Version = 1 },
+            new jedi{Id = 4, Name = "VinicinDoWoW", Strength = 130, Version = 1 }
+        };
+            _repositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(jediMuitos);
+
+            var result = await _service.GetAllAsync();
+
+            Assert.Equal(4, result.Count);
+        }
+    }
+}
---
+++ JediApi/Repositories/IJediRepository.cs
@@ -0,0 +1,13 @@
+﻿using JediApi.Models;
+
+namespace JediApi.Repositories
+{
+    public interface IJediRepository
+    {
+        Task<jedi> GetByIdAsync(int id);
+        Task<List<jedi>> GetAllAsync();
+        Task<jedi> AddAsync(jedi jedi);
+        Task<bool> UpdateAsync(jedi jedi);
+        Task<bool> DeleteAsync(int id);
+    }
+}
